0599813d0bfb32b7dd93f2c9ea85e8f6711a5068
kegranados1@gmail.com <kegranados1@gmail.com>
2018-07-23 21:51:22 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,14 @@
+function makeArray() {
+  const array = []
+  const t = Math.floor(Math.random() * 10)
+
+  for (let i = 1; i < t; i++) {
+    array.push("I am 1 strange loop.")
+  } else {
+    array.push("I am ${i} strange loop.")
+  }
+let countdown = n
+const n = Math.floor(Math.random()*100)
+while (countdown > 0) {
+  console.log(--countdown)
+}
